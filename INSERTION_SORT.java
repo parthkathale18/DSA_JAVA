db96d1fbefae8d35cad8948e4195f1f948767699
parthkathale18 <150028426+parthkathale18@users.noreply.github.com>
2024-03-22 21:01:10 +0530

    Add files via upload

---
+++ INSERTION_SORT.java
@@ -0,0 +1,25 @@
+public class INSERTION_SORT {
+
+    public static void printArray(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 7, 8, 3, 1, 2 };
+
+        for (int i = 1; i < a.length; i++) {
+            int current = a[i];
+            int j = i - 1;
+            while (j >= 0 && current < a[j]) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = current;
+
+        }
+        printArray(a);
+    }
+}
